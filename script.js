39e1ee33eb3242a36b298244c4322cb03c26fb30
laguna99999 <lookatme99999@yahoo.com>
2019-08-17 03:19:30 +1000

    Dropdown render

--- script.js
+++ script.js
@@ -60,7 +60,83 @@ function getRate(symbol) {
 function formatNumberToCurrencyType(num) {
   return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
 }
+// ------------------------------------ Custom code for making dropdown element --------------------------------------- //
 
-function drawDropdown(node){
+// ---  Style to pseudo elements  ---
+var UID = {
+	_current: 0,
+	getNew: function(){
+		this._current++;
+		return this._current;
+	}
+};
+
+HTMLElement.prototype.pseudoStyle = function(element,prop,value){
+	var _this = this;
+	var _sheetId = "pseudoStyles";
+	var _head = document.head || document.getElementsByTagName('head')[0];
+	var _sheet = document.getElementById(_sheetId) || document.createElement('style');
+	_sheet.id = _sheetId;
+	var className = "pseudoStyle" + UID.getNew();
+
+	_this.className +=  " "+className;
+
+	_sheet.innerHTML += " ."+className+":"+element+"{"+prop+":"+value+"}";
+	_head.appendChild(_sheet);
+	return this;
+};
+
+// -------
+
+function changeCurrency(currency){
 
 }
+
+// ------- Render dropdown --------
+
+var dropdown_parent = document.createElement("li");
+var dropdown_icon = document.createElement("i");
+var dropdown_text = document.createElement("div");
+
+var dropdown_children = document.createElement("nav");
+var children = ["דולר ($)", "אירו (€)", "שקלים (₪)"];
+
+setTimeout(function(){
+
+    dropdown_parent.style.position = "absolute";
+    dropdown_parent.style.right = "-50px";
+    dropdown_parent.style.top = "5px";
+    dropdown_parent.style.cursor = "pointer";
+    dropdown_text.innerText = "דולר ($)";
+
+    dropdown_icon.addClass("icon-contact");
+    dropdown_icon.style.paddingLeft = "15px";
+    dropdown_icon.style.marginTop = "5px";
+    dropdown_icon.style.color = "#d20018";
+    dropdown_icon.style.fontSize = "15px";
+    dropdown_parent.append(dropdown_icon);
+    dropdown_parent.append(dropdown_text);
+
+    dropdown_text.style.lineHeight = "14px";
+    document.getElementsByClassName("links-and-phones-section")[1].append(dropdown_parent);
+
+    for(var child of children){
+        var child_tag = document.createElement("li");
+        child_tag.pseudoStyle("hover", "background", "#ebebeb !important");
+        child_tag.style.borderBottom = "1px solid rgba(0, 0, 0, 0.12)";
+        child_tag.style.textAlign = "center";
+        child_tag.style.padding = "10px 20px";
+        child_tag.style.display = "block";
+        child_tag.style.color = "#222";
+        child_tag.style.background = "#fff"
+        child_tag.style.position = "relative";
+        child_tag.style.verticalAlign = "top";
+        child_tag.style.textDecoration = "none";
+        child_tag.style.marginLeft = "auto";
+        child_tag.style.marginRight = "auto";
+        child_tag.style.whiteSpace = "nowrap";
+        child_tag.innerText = child;
+        dropdown_children.append(child_tag);
+    }
+    dropdown_parent.append(dropdown_children);
+}, 3000);
